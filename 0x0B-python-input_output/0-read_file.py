2197b9ab86f389d89c3349eb9841b2ace8cc8661
Ubong <ubongoben@gmail.com>
2023-02-19 18:56:43 -0800

    my commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-function that reads a text file
-"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout
-    Returns none
-    """
-    with open(filename, "r", encoding="utf-8") as f:
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
